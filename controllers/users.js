7a5658f74a0c642382c6c5a87cdede251d6df27e
GlazkovPavel <glazprom@mail.ru>
2021-09-07 23:37:55 +0300

    feat: Сделал обработку ошибок

--- controllers/users.js
+++ controllers/users.js
@@ -2,14 +2,19 @@ const User = require('../models/user');
 
 module.exports.getUserId = (req, res) => {
   User.findById(req.params.id)
-    .then(user => res.send({data: user}))
-    .catch(err => res.status(500).send({message: 'Произошла ошибка'}))
+    .then((user) => {
+      if(!user){
+        res.status(404).send({ message: "Запрашиваемый пользователь не найден"})
+      }
+       res.status(200).send({data: user})
+    })
+    .catch((err) => res.status(500).send(`Произошла ошибка: ${err.name} ${err.message}`))
 }
 
 module.exports.getUsers = (req, res) => {
   User.find({})
     .then(users => res.status(200).send({data: users}))
-    .catch(err => res.status(500).send({message: err.message}));
+    .catch((err) => res.status(500).send(`Произошла ошибка: ${err.name} ${err.message}`));
 };
 
 module.exports.createUser = (req, res) => {
@@ -17,17 +22,23 @@ module.exports.createUser = (req, res) => {
 
   User.create({name, about, avatar})
     .then(user => res.status(201).send({data: user}))
-    .catch(err => res.status(500).send({message: err.message}));
+    .catch((err) => {
+      if(err.name || err.about || err.avatar === "ValidationError"){
+        res.status(400).send({ message: "Произошла ошибка валидации"}
+        )
+      }
+      res.status(500).send(`Произошла ошибка: ${err.name} ${err.message}`)
+    });
 };
 
 module.exports.updateUser = (req, res) =>{
   User.findByIdAndUpdate(req.user._id, {name: req.body.name, about: req.body.about})
     .then(user => res.status(200).send({data: user}))
-    .catch(err => res.status(500).send({message: err.message}));
+    .catch((err) => res.status(500).send(`Произошла ошибка: ${err.name} ${err.message}`));
 }
 
 module.exports.updateAvatar = (req, res) =>{
   User.findByIdAndUpdate(req.user._id, {name: req.body.avatar})
     .then(user => res.status(200).send({data: user}))
-    .catch(err => res.status(500).send({message: err.message}));
+    .catch((err) => res.status(500).send(`Произошла ошибка: ${err.name} ${err.message}`));
 }
