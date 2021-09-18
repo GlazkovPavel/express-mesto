10280e7ac74b6eb3ea1d657df23b780bdd7d7b58
GlazkovPavel <glazprom@mail.ru>
2021-09-18 21:41:26 +0300

    feat: Сделал хеш

--- controllers/users.js
+++ controllers/users.js
@@ -1,4 +1,6 @@
-const { NOT_FOUND_ERROR, INTERNAL_SERVER_ERROR, BAD_REQUEST_ERROR } = require('../errors/errors');
+const { NOT_FOUND_ERROR, INTERNAL_SERVER_ERROR, BAD_REQUEST_ERROR, EMAIL_CONFLICT} = require('../errors/errors');
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
 
 const User = require('../models/user');
 
@@ -24,20 +26,42 @@ module.exports.getUsers = (req, res) => {
 };
 
 module.exports.createUser = (req, res) => {
-  const { name = 'Жак-Ив Кусто',
+  const {
+    name = 'Жак-Ив Кусто',
     about = 'Исследователь',
     avatar = 'https://pictures.s3.yandex.net/resources/jacques-cousteau_1604399756.png',
     email,
     password} = req.body;
 
-  User.create({ name, about, avatar, email, password })
-    .then((user) => res.status(201).send({ data: user }))
-    .catch((err) => {
-      if (err.name === 'ValidationError') {
-        res.status(BAD_REQUEST_ERROR).send({ message: 'Произошла ошибка валидации' });
-      }
-      res.status(INTERNAL_SERVER_ERROR).send(`Произошла ошибка: ${err.name} ${err.message}`);
-    });
+  bcrypt.hash(password, saltRounds, (err, hash) => {
+    if(err) {
+      return res.status(INTERNAL_SERVER_ERROR).send(`Произошла ошибка: ${err.name} ${err.message}`)
+    }
+
+    User.findOne({ email })
+      .then((user) => {
+        if (!user) {
+         return User.create({ name, about, avatar, email, password: hash })
+            .then((u) => res.status(201).send({
+              _id: u._id,
+              name: u.name,
+              about: u.about,
+              avatar: u.avatar,
+            }))
+            .catch((err) => {
+              if (err.name === 'CastError') {
+                res.status(BAD_REQUEST_ERROR).send({ message: 'Произошла ошибка валидации' });
+              }
+              res.status(INTERNAL_SERVER_ERROR).send(`Произошла ошибка: ${err.name} ${err.message}`);
+            });
+        }
+        return res.status(EMAIL_CONFLICT).send({ message: 'Такой пользователь уже существует' });
+      })
+      .catch((err) => {
+        return res.status(INTERNAL_SERVER_ERROR).send(`Произошла ошибка: ${err.name} ${err.message}`);
+      });
+
+  })
 };
 
 module.exports.updateUser = (req, res) => {
--- models/user.js
+++ models/user.js
@@ -1,10 +1,11 @@
 const mongoose = require('mongoose');
+//const validator = require('validator');
 
 const userSchema = new mongoose.Schema({
   email: {
     type: String,
     required: true,
-    unique: true
+    unique: true,
   },
   password: {
     type: String,
